d84807c9e260673d94cc72b51bef49520e7a83ee
Tim Heilman <tdh@stanfordalumni.org>
2023-09-03 10:09:08 -0700

    chore: background color changed,
    https://docs.expo.dev/tutorial/build-a-screen/

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 // template from https://docs.expo.dev/tutorial/introduction/
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-      <View style={styles.container}>
-        <Text>Hello world!</Text>
-        {/*was from create-expo-app: */}
-        {/*<StatusBar style="auto" />*/}
-      </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
