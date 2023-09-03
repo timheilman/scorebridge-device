31fd623d4d71b7a5438ea062edc64303616af4c0
Tim Heilman <tdh@stanfordalumni.org>
2023-09-03 14:32:31 -0700

    chore: tutorial, screen capture

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import * as ImagePicker from "expo-image-picker";
+import * as MediaLibrary from "expo-media-library";
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ImageSourcePropType, StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { captureRef } from "react-native-view-shot";
 
 import Button from "./components/Button";
 import CircleButton from "./components/CircleButton";
@@ -17,12 +19,20 @@ const PlaceholderImage =
   require("./assets/images/background-image.png") as { uri: string };
 
 export default function App() {
+  const imageRef = useRef<View | null>(null);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [showAppOptions, setShowAppOptions] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | null>(
     null,
   );
+  if (status === null) {
+    requestPermission().catch((e: { message: string }) =>
+      alert(`Problem requesting permission ${e.message}`),
+    );
+  }
+
   const pickImageAsync = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -54,20 +64,40 @@ export default function App() {
     setIsModalVisible(false);
   };
 
-  const onSaveImageAsync = () => {
-    // we will implement this later
-  };
+  const onSaveImageAsync = async () => {
+    console.log("Does this go to the dev server's stdout?");
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
 
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Saved!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const dispatchSaveImage = () => {
+    console.log("how bout dat");
+    onSaveImageAsync().catch(() => {
+      alert(`Unreachable; try-catch would have caught`);
+    });
+  };
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer
-          placeholderImageSource={PlaceholderImage}
-          selectedImage={selectedImage}
-        />
-        {pickedEmoji !== null ? (
-          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-        ) : null}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer
+            placeholderImageSource={PlaceholderImage}
+            selectedImage={selectedImage}
+          />
+          {pickedEmoji !== null ? (
+            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          ) : null}
+        </View>
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
@@ -77,7 +107,7 @@ export default function App() {
             <IconButton
               icon="save-alt"
               label="Save"
-              onPress={onSaveImageAsync}
+              onPress={dispatchSaveImage}
             />
           </View>
         </View>
