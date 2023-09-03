ce9c91612e225eacc59aef910c7a94e466d07f79
Tim Heilman <tdh@stanfordalumni.org>
2023-09-03 11:16:41 -0700

    chore: tutorial thru gestures step 3, spring animation on emoji resize

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,13 +1,38 @@
 import { View, Image } from 'react-native';
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+import { TapGestureHandler } from 'react-native-gesture-handler';
+import Animated, {
+    useAnimatedStyle,
+    useSharedValue,
+    useAnimatedGestureHandler,
+    withSpring,
+} from 'react-native-reanimated';
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+    const scaleImage = useSharedValue(imageSize);
+    const onDoubleTap = useAnimatedGestureHandler({
+        onActive: () => {
+            if (scaleImage.value !== imageSize * 2) {
+                scaleImage.value = scaleImage.value * 2;
+            }
+        },
+    });
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value),
+        };
+    });
+
     return (
         <View style={{ top: -350 }}>
-            <Image
-                source={stickerSource}
-                resizeMode="contain"
-                style={{ width: imageSize, height: imageSize }}
-            />
+            <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+                <AnimatedImage
+                    source={stickerSource}
+                    resizeMode="contain"
+                    style={[imageStyle, { width: imageSize, height: imageSize }]}
+                />
+            </TapGestureHandler>
         </View>
     );
 }
