568ac59ab5df02c0a1c45fc00cb6ec261920d45b
Tim Heilman <tdh@stanfordalumni.org>
2023-09-03 10:12:17 -0700

    refactor: extract imageViewer to own component, first component

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-// template from https://docs.expo.dev/tutorial/introduction/
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text style={{color: "#fff"}}>Open up App.js to start working on your app!</Text>
+            <View style={styles.imageContainer}>
+                <ImageViewer placeholderImageSource={PlaceholderImage} />
+            </View>
             <StatusBar style="auto" />
         </View>
     );
 }
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#25292e',
         alignItems: 'center',
-        justifyContent: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 58,
     },
 });
+
